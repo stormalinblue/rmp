52d4f0267445225f130e3e0e6b43253f824ed70a
Muktaka Joshipura <muktaka2000@gmail.com>
2026-01-21 07:48:14 -0600

    feat: Add BigUInt : FromStr

--- src/biguint/convert.rs
+++ src/biguint/convert.rs
@@ -1,6 +1,7 @@
 use super::basic::BigUInt;
+use std::str::FromStr;
 
-pub enum ConvertError {
+pub enum ConvertOutNumError {
     WouldOverflow,
 }
 
@@ -19,13 +20,46 @@ impl From<u64> for BigUInt {
 }
 
 impl TryFrom<&BigUInt> for u64 {
-    type Error = ConvertError;
+    type Error = ConvertOutNumError;
 
-    fn try_from(n: &BigUInt) -> Result<u64, ConvertError> {
+    fn try_from(n: &BigUInt) -> Result<u64, ConvertOutNumError> {
         match n.limbs.len() {
             0 => Ok(0),
             1 => Ok(n.limbs[0]),
-            _ => Err(ConvertError::WouldOverflow),
+            _ => Err(ConvertOutNumError::WouldOverflow),
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct ParseBigUIntError {}
+
+impl FromStr for BigUInt {
+    type Err = ParseBigUIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // Optimization: Batching
+
+        let mut result = BigUInt::zero();
+        for c in s.chars() {
+            if !('0' <= c && c <= '9') {
+                return Err(ParseBigUIntError {});
+            } else {
+                let digit = (c as u64) - ('0' as u64);
+                result *= 10u32;
+                result += digit;
+            }
+        }
+        Ok(result)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::biguint::BigUInt;
+
+    #[test]
+    fn from_str_small() {
+        assert_eq!("1234".parse::<BigUInt>().unwrap(), BigUInt::from(1234u64));
     }
 }
